b6223b6d2018726b391ef1cfe7df6c08d51f6f59
Tertiium <yang.guozhang@yahoo.com>
2017-09-04 19:35:26 -0300

    6 Commit - Criado interface de cadastro e login

--- Snapchat/CadastroViewController.swift
+++ Snapchat/CadastroViewController.swift
@@ -1,27 +1,29 @@
 //
-//  ViewController.swift
+//  CadastroViewController.swift
 //  Snapchat
 //
-//  Created by user on 31/08/17.
+//  Created by user on 04/09/17.
 //  Copyright © 2017 yangguozhang. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CadastroViewController: UIViewController {
+    
     // MARK: - Properties
     
     // MARK: - Outlets
     
     // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        self.navigationController?.setNavigationBarHidden(true, animated: false)
+        self.navigationController?.setNavigationBarHidden(false, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,9 +31,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    // MARK: - Actions
     
 
-
 }
-
