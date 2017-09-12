7ffce73c6a0ec5364bcc9f77573351a8dbacb6f5
Tertiium <yang.guozhang@yahoo.com>
2017-09-12 17:59:25 -0300

    7 Commit - Cadastro de usuario

--- Snapchat/CadastroViewController.swift
+++ Snapchat/CadastroViewController.swift
@@ -7,13 +7,18 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class CadastroViewController: UIViewController {
     
     // MARK: - Properties
     
     // MARK: - Outlets
+    @IBOutlet weak var textFieldEmail: UITextField!
     
+    @IBOutlet weak var textFieldSenha: UITextField!
+    
+    @IBOutlet weak var textFieldConfirmarSenha: UITextField!
     // MARK: - Life Cycle
 
     override func viewDidLoad() {
@@ -29,6 +34,45 @@ class CadastroViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - IBAction
+    
+    func exibirMensagem(titulo: String, mensagem: String) {
+        let alerta = UIAlertController(title: titulo, message: mensagem, preferredStyle: .alert)
+        let acaoCancelar = UIAlertAction(title: "Cancelar", style: .cancel, handler: nil)
+        alerta.addAction(acaoCancelar)
+        self.present(alerta, animated: true, completion: nil)
+    }
+    
+    @IBAction func criarConta(_ sender: UIButton) {
+        
+        // Recuperar dados digitados
+        if let emailR = self.textFieldEmail.text {
+            if let senhaR = self.textFieldSenha.text {
+                if let senhaConfirmacaoR = self.textFieldConfirmarSenha.text {
+                    
+                    // Validar senha
+                    if senhaR == senhaConfirmacaoR {
+                        
+                        // Criar conta no Firebase
+                        let autenticacao = FIRAuth.auth()
+                        autenticacao?.createUser(withEmail: emailR, password: senhaR, completion: { (user, error) in
+                            if error == nil {
+                                print("Sucesso ao cadastrar usuário.")
+                            }else {
+                                print("Erro ao cadastrar usuário")
+                            }
+                        })
+                        
+                    }else{
+                        self.exibirMensagem(titulo: "Dados incorretos.", mensagem: "As senhas não estão iguais, digite novamente.")
+                    }/* Fim Validação senha*/
+                    
+                }
+            }
+        }
+        
     }
     
     
