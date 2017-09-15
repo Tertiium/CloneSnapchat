53cbc9c85631cfc98c3e024e565666cf89648f1c
Tertiium <yang.guozhang@yahoo.com>
2017-09-15 18:55:30 -0300

    12 Commit - Selecionar fotos

--- Snapchat/FotoViewController.swift
+++ Snapchat/FotoViewController.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 
-class FotoViewController: UIViewController {
+class FotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // MARK: - Properties
+
+    var imagePicker = UIImagePickerController()
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var imageViewFoto: UIImageView!
+    
+    @IBOutlet weak var textFieldDescricao: UITextField!
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +35,24 @@ class FotoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: - IBActions
+    
+    @IBAction func selecionarFoto(_ sender: UIBarButtonItem) {
+        
+        imagePicker.sourceType = .savedPhotosAlbum
+        present(imagePicker, animated: true, completion: nil)
+        
+        
     }
-    */
+    
+    // MARK: - Functions
+    
+    func  imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let imagemRecuperada = info[UIImagePickerControllerOriginalImage] as! UIImage
+        self.imageViewFoto.image = imagemRecuperada
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 
 }
